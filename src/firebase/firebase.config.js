a4d0ec78c968568a982bd1b06c549dac6e1bd04f
Apu4142 <devopu988@gmail.com>
2025-10-23 19:43:02 +0600

    add firbase Auth:

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2Fww4ijz4NdBVUr-LlsgDczGkyA3eHa0",
+  authDomain: "green-nest-plant-auth.firebaseapp.com",
+  projectId: "green-nest-plant-auth",
+  storageBucket: "green-nest-plant-auth.firebasestorage.app",
+  messagingSenderId: "507335944060",
+  appId: "1:507335944060:web:4e8ec1cc327137bf1566f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
