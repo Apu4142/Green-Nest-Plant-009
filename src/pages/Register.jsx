c8e613a0ac532179ff5b9d51fb8a75e3eef605c1
Apu4142 <devopu988@gmail.com>
2025-10-23 19:23:40 +0600

    add Register input

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
     return (
-        <div>
-            register
-        </div>
+          <div className="flex justify-center items-center min-h-screen">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <h2 className="font-bold text-2xl text-center mt-12 border-b-2 pb-5 border-gray-200 mx-5">
+          Register your account
+        </h2>
+        <form  className="card-body">
+          <fieldset className="fieldset">
+            {/* Name */}
+            <label className="label">Name</label>
+            <input
+              name="name"
+              type="text"
+              className="input"
+              placeholder="Name"
+              required
+            />
+           
+            {/*Photo photo */}
+            <label className="label">Photo photo</label>
+            <input
+              name="photo"
+              type="text"
+              className="input"
+              placeholder="Photo photo"
+              required
+            />
+            {/* email */}
+            <label className="label">Email</label>
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+              required
+            />
+
+            {/* password */}
+            <label className="label">Password</label>
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
+
+            <button type="submit" className="btn btn-neutral mt-4">
+              Register
+            </button>
+            <p className="font-bold text-center pt-2">
+              Already Have An Account ?{" "}
+              <Link className="text-secondary" to={"/auth/login"}>
+                Login
+              </Link>
+            </p>
+          </fieldset>
+        </form>
+      </div>
+    </div>
     );
 };
 
